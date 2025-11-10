b0eb6de2ff48b18d074476d71a356e13ee08f08f
AdiiiAdiAdi <Aditya.muddana@mitwpu.edu.in>
2025-11-11 00:55:49 +0530

    Update ass4.cpp

--- ass4.cpp
+++ ass4.cpp
@@ -1,72 +1,59 @@
 #include <stdio.h>
-#include <climits>
 
-int ST[5], AT[5], TAT[5], CT[5], WT[5], BT[5], RT[5];
+int ST[5], AT[5], TAT[5], CT[5], WT[5], BT[5];     //start, arrival, turn around, completion, wait, burst
+// tat is the total time taken including wait time in queue               //        <--WT--> <--BT-->
+                                                                          //        AT      ST      CT
+                                                                          //        <-----TAT------->
 
-void processtime(int AT[], int BT[]) {
-    int complete = 0,  i;
-    for (i = 1; i < 7; i++) {
-        RT[i] = BT[i];
-    }
-int s;
-int min;
-int time = 0;
-    while (complete < 6) {
-        s = 0;
-        min = INT_MAX;
+void processtime() {
+   
+    ST[0] = AT[0];   //for first process its start is when it arrived            
+    CT[0] = ST[0] + BT[0]; 
+    TAT[0] = CT[0] - AT[0];      
+    WT[0] = TAT[0] - BT[0];
 
-        for (i = 1; i < 7; i++) {
-            if (AT[i] <= time && RT[i] > 0 && RT[i] < min) {
-                min = RT[i];
-                s = i;
-            }
-        }
-
-
-	
-       			 if (RT[s] == BT[s]) {
-           			 ST[s] = time;
-       						 }
-
-       		 RT[s]--;
-       		 time++;
-
-      if (RT[s] == 0) {
-          CT[s] = time;
-          TAT[s] = CT[s] - AT[s];
-          WT[s] = TAT[s] - BT[s];
-          complete++;
-        }
+    
+   for (int i = 1; i < 4; i++) {
+        ST[i] = CT[i - 1];        // next process starts when previous finishes
+        CT[i] = ST[i] + BT[i]; 
+        TAT[i] = CT[i] - AT[i]; 
+        WT[i] = TAT[i] - BT[i]; 
     }
 }
 
 int main() {
-    for (int i = 1; i < 7; i++) {
-        printf("What is process %d's arrival time? ", i);
+    
+    for (int i = 0; i < 4; i++) {         // for 4 processes
+        printf("What is process %d's arrival time? ", i + 1);
         scanf("%d", &AT[i]);
-        printf("What is process %d's burst time? ", i);
+
+        printf("What is process %d's burst time? ", i + 1);
         scanf("%d", &BT[i]);
     }
 
-    processtime(AT, BT);
-
-    int avg_wt = 0, avg_tat = 0;
-    for (int i = 1; i <= 6; i++) {
-        avg_wt += WT[i];
-        avg_tat += TAT[i];
+    // Calculate times
+    processtime();
+    
+    int avg_wt = 0;
+    int avg_tnt = 0;
+    
+    for (int p = 0; p < 4; p++) {
+        avg_wt += WT[p];
+        avg_tnt += TAT[p];
     }
-
+   
     avg_wt = avg_wt / 4;
-    avg_tat = avg_tat / 4;
+    avg_tnt = avg_tnt / 4;
 
-    printf("Process   AT  BT  ST  CT  TAT  WT\n");
-    for (int i = 1; i <= 6; i++) {
-        printf("%d\t   %d   %d   %d   %d   %d   %d\n", i, AT[i], BT[i], ST[i], CT[i], TAT[i], WT[i]);
+    // Print the results
+    printf("\nProcess   Arrival Time Burst Time Start Time  Completion Time  Turnaround Time  Waiting Time\n");
+    for (int i = 0; i < 4; i++) {
+        printf("%d\t\t %d\t\t  %d\t  %d\t\t  %d\t\t %d\t\t  %d\n",  
+               i + 1, AT[i], BT[i], ST[i], CT[i], TAT[i], WT[i]);
     }
-
-    printf("Average Waiting Time: %d\n", avg_wt);
-    printf("Average Turnaround Time: %d\n", avg_tat);
-
+    
+    printf("\nAverage wait time is: %d", avg_wt);
+    printf("\nAverage turn around time is: %d\n", avg_tnt);
+    
     return 0;
 }
-
